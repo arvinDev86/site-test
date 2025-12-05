957104ad629936d3e6b5916052da82fd6e5363f7
Arvinb1386 <bagheriyanarvin3@gmail.com>
2025-12-05 15:18:06 +0330

    update

--- index.tsx
+++ index.tsx
@@ -1,22 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/REPO-NAME">
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter basename="/site-test/">
     <App />
   </BrowserRouter>
 )
